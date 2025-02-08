c4b552ccc54c235afa97e813e0ab2e64486719d0
vlad <fk8ghtt@gmail.com>
2025-02-08 18:02:11 +0300

    1.3.1
    Sync to the home laptop

--- Project/Project/obj/Debug/net8.0-windows/Project.AssemblyInfo.cs
+++ Project/Project/obj/Debug/net8.0-windows/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b161746b73d624b1acfadbe68e6a08f60b69e48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614598db82e228db39a2a817b48f67ccfeca7b6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
